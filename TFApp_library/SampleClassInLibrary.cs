0a6d11cc80638e5920875f8bc19c9038c0776b39
Hidekazu Yokono <h-yokono@alterbooth.com>
2023-08-30 13:47:41 +0900

    [modify] Add workflow to upload nuget package

--- TFApp_library/SampleClassInLibrary.cs
+++ TFApp_library/SampleClassInLibrary.cs
@@ -2,7 +2,5 @@
 
 public class SampleClassInLibrary
 {
-    private static readonly string SAMPLE_MESSAGE = "12Factor App sample library message";
-    
     public static string Message = "(updated) 12Factor App sample library message!";
 }
