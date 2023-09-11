49e6b87c78239f39be8a1a17e0c7fe52c8501811
Hidekazu Yokono <h-yokono@alterbooth.com>
2023-09-11 17:37:37 +0900

    [modify] SampleClassInLibraryの変更

--- TFApp_library/SampleClassInLibrary.cs
+++ TFApp_library/SampleClassInLibrary.cs
@@ -2,9 +2,5 @@
 
 public class SampleClassInLibrary
 {
-    // public static string Message = "(updated) 12Factor App sample library message!";
-
-    private static readonly string SAMPLE_MESSAGE = "(updated) 12Factor App sample library message!";
-    
-    public string Message => SAMPLE_MESSAGE;
+    public static string Message = "(updated) 12Factor App sample library message!";
 }
